f62b632d4b178e4bba62018142369413884571d8
Dylan Tweebeeke <dylan.tweebeeke@zoho.com>
2019-11-21 12:06:35 +0100

    Added Home Screen
    Added the home screen (NOT DONE), also added some code which makes a minimum window size (TODO: add this piece of code in every screen so it's every screen has the same minimal size)

--- src/GUI/homeScreen.java
+++ src/GUI/homeScreen.java
@@ -1,4 +1,44 @@
 package GUI;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
 public class homeScreen {
+    private  JLabel greetingText;
+    private JButton wekelijksButton;
+    private JButton maandelijksButton;
+    private JPanel homeView;
+    private JLabel homeText;
+    private JLabel subTitleText;
+    private JButton dagelijksVerbruikButton;
+
+    public static void main(String[] args) {
+        //creating new Jframe
+        JFrame frame = new JFrame("Homescreen");
+        frame.setContentPane(new homeScreen().homeView);
+
+        //Making the window able to close
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+
+        //defining minimum and maximum size of the windwow.
+        frame.setMaximumSize(new Dimension(440,500));
+        frame.addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent evt) {
+                Dimension size = frame.getSize();
+                Dimension min = frame.getMinimumSize();
+                if (size.getWidth() < min.getWidth()) {
+                    frame.setSize((int) min.getWidth(), (int) size.getHeight());
+                }
+                if (size.getHeight() < min.getHeight()) {
+                    frame.setSize((int) size.getWidth(), (int) min.getHeight());
+                }
+            }
+        });
+        frame.setSize(400, 500);
+        //setting the frame to be visible
+        frame.setVisible(true);
+    }
 }
