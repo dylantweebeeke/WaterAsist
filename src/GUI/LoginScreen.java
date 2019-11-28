eb1f94a802784724a621cbe8721ff2b308078959
Dylan Tweebeeke <dylan.tweebeeke@zoho.com>
2019-11-28 11:02:08 +0100

    updated login screen and added dummy login compare

--- src/GUI/LoginScreen.java
+++ src/GUI/LoginScreen.java
@@ -1,18 +1,52 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class LoginScreen {
     public JPanel LoginPanel;
-    private JTextField textField1;
+    private JTextField usernameTextfield;
     private JButton loginButton;
 
+
+    public LoginScreen() {
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (usernameTextfield.getText().equals("test")) {
+                    Component component = (Component) e.getSource();
+                    JFrame frame = (JFrame) SwingUtilities.getRoot(component);
+                    frame.setContentPane(new homeScreen().homeView);
+                    frame.setVisible(true);
+                    System.out.println("New window opened!");
+                }
+            }
+        });
+    }
 
     public static void main(String args[]) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
         JFrame frame = new JFrame("Login");
         frame.setContentPane(new LoginScreen().LoginPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setMaximumSize(new Dimension(440,500));
+        frame.addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent evt) {
+                Dimension size = frame.getSize();
+                Dimension min = frame.getMinimumSize();
+                if (size.getWidth() < min.getWidth()) {
+                    frame.setSize((int) min.getWidth(), (int) size.getHeight());
+                }
+                if (size.getHeight() < min.getHeight()) {
+                    frame.setSize((int) size.getWidth(), (int) min.getHeight());
+                }
+            }
+        });
+        frame.setSize(1100, 1100);
         frame.setVisible(true);
     }
 }
